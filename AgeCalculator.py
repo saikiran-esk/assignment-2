22dde07ee5b7d4160015d9abad059ae621e19db8
saikiran-esk <eski22cs@cmrit.ac.in>
2026-02-21 10:55:13 +0530

    AgeCalculator

---
+++ AgeCalculator.py
@@ -0,0 +1,13 @@
+year=int(input("Enter year of birth:"))
+Age=2026-year
+months=Age*12
+Days=Age*365
+hours=Days*24
+minutes=hours*60
+years_until_100=100-Age
+print(f"Current Age is : {Age}")
+print(f"Age in months:{months}")
+print(f"Age in days : {Days}")
+print(f"Age in hours : {hours}")
+print(f"Age in minutes : {minutes}")
+print(f"Years until 100 :{100-Age}")
