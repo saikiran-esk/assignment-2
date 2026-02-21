2006c872e131bdd77a5ac6d08c26293a2ac15e39
saikiran-esk <eski22cs@cmrit.ac.in>
2026-02-21 23:17:34 +0530

    again commit

---
+++ TextanalysisFunctions.py
@@ -0,0 +1,10 @@
+def count_words(text):
+    words=text.split()
+    print(f"words:{len(words)}")
+def count_vowels(text):
+    
+    print(f"vowels:{count}")
+
+
+count_words("sai the kiran")
+count_vowels("sai the kiran")
